37494d7c9001ca5417b9fb710d0f80aaa77c4045
nurba <kazitech2023@gmail.com>
2025-10-18 17:53:32 +0500

    commit fixed

--- resources/views/layouts/referee.blade.php
+++ resources/views/layouts/referee.blade.php
@@ -8,6 +8,14 @@
 
     <!-- Styles -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {},
+            },
+        }
+    </script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!-- Livewire Styles -->
@@ -60,6 +68,7 @@
     @livewireScripts
 
     @stack('scripts')
+
     <!-- Initialize theme -->
     <script>
         // Initialize theme immediately
